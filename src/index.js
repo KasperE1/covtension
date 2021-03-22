f05e1cbbef38e0975388467328f920bb03903afc
Kasper <35449118+KasperE1@users.noreply.github.com>
2021-03-22 12:48:19 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -1,42 +1,45 @@
-import axios from "axios";
-const api = "https://api.sl.se/api2/realtimedeparturesV4.json?key=4e65fe39f8304f1f955283cf428ed3ec&siteid=3404&timewindow=15";
-const errors = document.querySelector(".errors");
-const loading = document.querySelector(".loading");
-const cases = document.querySelector(".buses");
-const recovered = document.querySelector(".recovered");
-const deaths = document.querySelector(".deaths");
-const results = document.querySelector(".result-container");
-results.style.display = "none";
-loading.style.display = "none";
-errors.textContent = "";
-// grab the form
-const form = document.querySelector(".form-data");
-// grab the country name
-const country = document.querySelector(".country-name");
+// api url 
+const api_url = 
+	"https://api.sl.se/api2/realtimedeparturesV4.json?key=4e65fe39f8304f1f955283cf428ed3ec&siteid=3404&timewindow=15"; 
 
-// declare a method to search by country name
-const searchForCountry = async countryName => {
-  loading.style.display = "block";
-  errors.textContent = "";
-  try {
-    const response = await axios.get(`${api}/${countryName}`);
-    loading.style.display = "none";
-    cases.textContent = response.data.confirmed.value;
-    recovered.textContent = response.data.recovered.value;
-    deaths.textContent = response.data.deaths.value;
-    results.style.display = "block";
-  } catch (error) {
-    loading.style.display = "none";
-    results.style.display = "none";
-    errors.textContent = "We have no data for the country you have requested.";
-  }
-};
+// Defining async function 
+async function getapi(url) { 
+	
+	// Storing response 
+	const response = await fetch(url); 
+	
+	// Storing data in form of JSON 
+	var data = await response.json(); 
+	console.log(data); 
+	if (response) { 
+		hideloader(); 
+	} 
+	show(data); 
+} 
+// Calling that async function 
+getapi(api_url); 
 
-// declare a function to handle form submission
-const handleSubmit = async e => {
-  e.preventDefault();
-  searchForCountry(country.value);
-  console.log(country.value);
-};
-
-form.addEventListener("submit", e => handleSubmit(e));
+// Function to hide the loader 
+function hideloader() { 
+	document.getElementById('loading').style.display = 'none'; 
+} 
+// Function to define innerHTML for HTML table 
+function show(data) { 
+	let tab = 
+		`<tr> 
+		<th>Destination</th> 
+		<th>Linje</th> 
+		<th>Tid</th>
+		</tr>`; 
+	
+	// Loop to access all rows 
+	for (let r of data.list) { 
+		tab += `<tr> 
+	<td>${r.destination} </td> 
+	<td>${r.linenumber}</td> 
+	<td>${r.displaytime}</td>	 
+</tr>`; 
+	} 
+	// Setting innerHTML as tab variable 
+	document.getElementById("employees").innerHTML = tab; 
+} 
