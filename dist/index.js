f77d14f6e526bdff2d354fa51fa255688c7dbe6b
Kasper <35449118+KasperE1@users.noreply.github.com>
2021-03-25 14:38:01 +0100

    Update

--- dist/index.js
+++ dist/index.js
@@ -1,41 +1,50 @@
-const app = document.getElementById('root');
+// const app = document.getElementById('root');
 
-const logo = document.createElement('img');
-logo.src = 'logo.png';
+// const logo = document.createElement('img');
+// logo.src = 'logo.png';
 
-const container = document.createElement('div');
-container.setAttribute('class', 'container');
+// const container = document.createElement('div');
+// container.setAttribute('class', 'container');
 
-app.appendChild(logo);
-app.appendChild(container);
+// app.appendChild(logo);
+// app.appendChild(container);
 
-var request = new XMLHttpRequest();
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
-request.onload = function () {
+// var request = new XMLHttpRequest();
+// request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+// request.onload = function () {
 
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
-      const card = document.createElement('div');
-      card.setAttribute('class', 'card');
+//   // Begin accessing JSON data here
+//   var data = JSON.parse(this.response);
+//   if (request.status >= 200 && request.status < 400) {
+//     data.forEach(movie => {
+//       const card = document.createElement('div');
+//       card.setAttribute('class', 'card');
 
-      const h1 = document.createElement('h1');
-      h1.textContent = movie.title;
+//       const h1 = document.createElement('h1');
+//       h1.textContent = movie.title;
 
-      const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
+//       const p = document.createElement('p');
+//       movie.description = movie.description.substring(0, 300);
+//       p.textContent = `${movie.description}...`;
 
-      container.appendChild(card);
-      card.appendChild(h1);
-      card.appendChild(p);
-    });
-  } else {
-    const errorMessage = document.createElement('marquee');
-    errorMessage.textContent = `Gah, it's not working!`;
-    app.appendChild(errorMessage);
-  }
-}
+//       container.appendChild(card);
+//       card.appendChild(h1);
+//       card.appendChild(p);
+//     });
+//   } else {
+//     const errorMessage = document.createElement('marquee');
+//     errorMessage.textContent = `Gah, it's not working!`;
+//     app.appendChild(errorMessage);
+//   }
+// }
 
-request.send();
+// request.send();
+
+var url = "https://api.sl.se/api2/realtimedeparturesV4.json?key=4e65fe39f8304f1f955283cf428ed3ec&siteid=3404&timewindow=15";
+fetch(url, {
+  mode: "no-cors"
+}).then(response => {
+  return response.json();
+}).then(data => {
+  console.log(data);
+})
